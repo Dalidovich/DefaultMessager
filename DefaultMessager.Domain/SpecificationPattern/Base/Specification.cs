b994f3b0e0b5e75f9b5c4440dcd43ecceb021eb2
Ilia <pops375@yandex.ru>
2023-02-07 19:59:57 +0300

    release base Specification Pattern

---
+++ DefaultMessager.Domain/SpecificationPattern/Base/Specification.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace DefaultMessager.Domain.SpecificationPattern.Base
+{
+    public abstract class Specification<T> : ISpecification<T>
+    {
+        public abstract Expression<Func<T, bool>> ToExpression();
+
+        protected Expression<Func<T, bool>> expression = null;
+
+        public bool IsSatisfiedBy(T entity)
+        {
+            Func<T, bool> predicate = ToExpression().Compile();
+            return predicate(entity);
+        }
+    }
+}
