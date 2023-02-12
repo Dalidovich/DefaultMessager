1736b4d9e72218b461d307f516033442dc241e06
brikez <brike@DESKTOP-F74V8DB>
2023-02-12 23:12:26 +0300

    Rename table uesrs to accounts

--- DefaultMessager.DAL/Migrations/20230212190114_RenameTableUsersToAccounts.Designer.cs
+++ DefaultMessager.DAL/Migrations/20230212190114_RenameTableUsersToAccounts.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DefaultMessager.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DefaultMessager.DAL.Migrations
 {
     [DbContext(typeof(MessagerDbContext))]
-    partial class MessagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230212190114_RenameTableUsersToAccounts")]
+    partial class RenameTableUsersToAccounts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DefaultMessager.DAL/Migrations/20230212190114_RenameTableUsersToAccounts.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DefaultMessager.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTableUsersToAccounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "users",
+                newName: "accounts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                            name: "accounts",
+                            newName: "users");
+        }
+    }
+}
